a936898059c31873a6f3bcb4730d9b87dda4b62f
Romain <romain.chaumont@hotmail.fr>
2023-10-04 14:32:48 +0200

    add data in bdd

--- front-end-react/src/routes/root.jsx
+++ front-end-react/src/routes/root.jsx
@@ -1,11 +1,15 @@
 import NavigationBar from "../components/NavigationBar.jsx";
 import {Outlet} from "react-router-dom";
+import MyComponent from "../components/Test.jsx";
+import DataForm from "../components/Data.jsx";
 
 export default function Root() {
     return (
         <>
             <NavigationBar />
             <main>
+                <DataForm />
+                {/* <MyComponent /> */}
                 <Outlet />
             </main>
         </>
