3e72419be7b7ca9a89147f01532e88d5bf173cf4
zbrwkq <81628036+zbrwkq@users.noreply.github.com>
2023-10-04 16:34:44 +0200

    Graphique en reponse HTML de l'api et affichage sur interface uttilisateur (#10)

--- front-end-react/src/components/views/CrashByCountryGraphicView.jsx
+++ front-end-react/src/components/views/CrashByCountryGraphicView.jsx
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function CrashByCountryGraphicView() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        // Fonction qui va chercher les données depuis l'API
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('http://localhost:8080/api/countries');
+            setData(response.data);
+          } catch (error) {
+            console.error('Erreur lors de la récupération des données:', error);
+          }
+        };
+    
+        fetchData();
+      }, []); 
+    
+    
+    
     return (
         <>
             <h1 className="text-center">Crash by countries</h1>
             <div className="w-75 border rounded mx-auto mt-3" style={{height: "75vh"}}>
+                {data != null ? (
+                    <iframe srcDoc={data} style={{ width: '100%', height: '500px', border: 'none' }}/>
 
+                ) : (
+                    <h1>Chargement</h1>
+                )}
             </div>
         </>
     )
