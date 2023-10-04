d35f3d17b31ac5c61982713099ea0726282e4872
Romain <romain.chaumont@hotmail.fr>
2023-10-04 16:36:42 +0200

    OUHO

--- backend/file.py
+++ backend/file.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from flask_cors import CORS
@@ -17,24 +17,11 @@ class Crash(db.Model):
     Year = db.Column(db.String(45))
     Deaths = db.Column(db.String(45))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    sql_query = text("SELECT Country FROM road_accident_clean")
-    result = db.session.execute(sql_query)
-    data = [{'Country': row.Country} for row in result]
-    return jsonify(data)
-
 @app.route('/api/add_data', methods=['POST'])
 def add_data():
-    data = request.json  # Récupérez les données du formulaire envoyées depuis React
+    data = request.json
 
-    # Insérez les données dans votre base de données ici
-    # Exemple : Insérez les données dans le modèle Crash
     new_data = Crash(
         Category=data['category'],
         Country=data['country'],
