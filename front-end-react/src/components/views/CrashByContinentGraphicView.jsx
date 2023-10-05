cfd3386d328780f9779a2bb6f122c45e6a086f36
CorentinMaille <81628097+CorentinMaille@users.noreply.github.com>
2023-10-05 15:52:16 +0200

    - enorme fix (#22)
    Co-authored-by: Corentin Maille <maillecorentin@pm.me>

--- front-end-react/src/components/views/CrashByContinentGraphicView.jsx
+++ front-end-react/src/components/views/CrashByContinentGraphicView.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Button from "react-bootstrap/Button";
-import {FormControl, FormLabel, InputGroup} from "react-bootstrap";
+import {Container, FloatingLabel, FormControl, FormLabel, InputGroup, Row} from "react-bootstrap";
+import FormRange from "react-bootstrap/FormRange";
 
 function CrashByContinentGraphicView() {
     const [data, setData] = useState(null);
@@ -18,7 +19,7 @@ function CrashByContinentGraphicView() {
     }, [year]);
 
     function updateGraphic() {
-        const yearValue = document.getElementById('year').value;
+        const yearValue = document.getElementById('year-range').value;
         setYear(yearValue);
     }
 
@@ -32,14 +33,28 @@ function CrashByContinentGraphicView() {
                     <h1>Chargement</h1>
                 )}
 
-                <InputGroup>
-                    <FormLabel>Choisir une année</FormLabel>
-                    <FormControl id="year" type="number" min={1990} max={2019} />
-                </InputGroup>
+                {/*<FloatingLabel controlId="floatingLabel" label="Sélectionner une année" className="mx-auto w-50">*/}
+                {/*    <FormControl id="year" type="number"*/}
+                {/*                  min={1990} max={2019}*/}
+                {/*                  className=""*/}
+                {/*                  // style={{width: "250px"}}*/}
+                {/*                  placeholder="Sélectionner une année" />*/}
+                {/*</FloatingLabel>*/}
 
-                <Button id="submit" onClick={updateGraphic}>
-                     Valider
-                </Button>
+                {/*<label htmlFor="year-range" className="form-label">Sélectionner une année</label>*/}
+                {/*<input type="range" className="form-range" min="1990" max="2019" step="1" id="year-range"*/}
+                {/*       onChange={updateGraphic} />*/}
+                <Container>
+                    <Row>
+                        <FormLabel className={"text-center"}>Sélectionner une année</FormLabel>
+                    </Row>
+                    <Row>
+                        <h1 className={"text-center"}>{year}</h1>
+                    </Row>
+                    <Row>
+                        <FormRange className={"mx-auto w-75"} step="1" id="year-range" min="1990" max="2019" defaultValue={2019} onChange={updateGraphic} />
+                    </Row>
+                </Container>
             </div>
         </>
     );
